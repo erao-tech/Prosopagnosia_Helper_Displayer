cf29892e8fa411ec1eb9a0f7f86c4afe437f9d93
fredpan <fredpan0821@gmail.com>
2020-03-05 18:44:03 -0500

    Completed msg display by one click

--- app/src/main/java/tech/erao/prosopagnosiahelperdisplayer/MainActivity.java
+++ app/src/main/java/tech/erao/prosopagnosiahelperdisplayer/MainActivity.java
@@ -2,13 +2,27 @@ package tech.erao.prosopagnosiahelperdisplayer;
 
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.TextView;
 
 import com.vuzix.hud.actionmenu.ActionMenuActivity;
 
 public class MainActivity extends ActionMenuActivity {
+    private TextView user_name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        user_name = findViewById(R.id.user_name);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+//        single click for the touchpad
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            user_name.setText("Hi, my name is EraO");
+        }
+        return true;
     }
 }
