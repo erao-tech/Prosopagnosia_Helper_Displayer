1645fa8e380adbca54c801c96b0dd48b306ec6b8
fredpan <fredpan0821@gmail.com>
2020-03-06 00:27:22 -0500

    pure version: remove all the bluetooth codes

--- app/src/main/java/tech/erao/prosopagnosiahelperdisplayer/MainActivity.java
+++ app/src/main/java/tech/erao/prosopagnosiahelperdisplayer/MainActivity.java
@@ -1,88 +1,21 @@
 package tech.erao.prosopagnosiahelperdisplayer;
 
 
-import android.annotation.SuppressLint;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.view.KeyEvent;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.vuzix.hud.actionmenu.ActionMenuActivity;
 
 import java.io.ByteArrayOutputStream;
 
-import tech.erao.prosopagnosiahelperdisplayer.bluetooth.BluetoothChatService;
-import tech.erao.prosopagnosiahelperdisplayer.bluetooth.Constants;
-
 @SuppressWarnings("deprecation")
 public class MainActivity extends ActionMenuActivity {
     private TextView user_name;
     private static final int REQUEST_IMAGE_CAPTURE = 1;
-    @SuppressLint("HandlerLeak")
-    private final Handler mHandler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case Constants.MESSAGE_STATE_CHANGE:
-                    switch (msg.arg1) {
-                        case BluetoothChatService.STATE_CONNECTED:
-                            System.out.println("CONNECTED");
-                            Toast.makeText(MainActivity.this, "CONNECTED", Toast.LENGTH_LONG);
-//                            setStatus(getString(R.string.title_connected_to, mConnectedDeviceName));
-//                            mConversationArrayAdapter.clear();
-                            break;
-                        case BluetoothChatService.STATE_CONNECTING:
-                            System.out.println("CONNECTING");
-//                            setStatus(R.string.title_connecting);
-                            break;
-                        case BluetoothChatService.STATE_LISTEN:
-                            System.out.println("LISTEN");
-                        case BluetoothChatService.STATE_NONE:
-//                            setStatus(R.string.title_not_connected);
-                            break;
-                    }
-                    break;
-                case Constants.MESSAGE_WRITE:
-                    byte[] writeBuf = (byte[]) msg.obj;
-                    // construct a string from the buffer
-                    String writeMessage = new String(writeBuf);
-                    System.out.println(writeMessage);
-//                    mConversationArrayAdapter.add("Me:  " + writeMessage);
-                    break;
-                case Constants.MESSAGE_READ:
-                    byte[] readBuf = (byte[]) msg.obj;
-                    // construct a string from the valid bytes in the buffer
-                    String readMessage = new String(readBuf, 0, msg.arg1);
-//                    mConversationArrayAdapter.add(mConnectedDeviceName + ":  " + readMessage);
-                    break;
-                case Constants.MESSAGE_DEVICE_NAME:
-                    // save the connected device's name
-//                    mConnectedDeviceName = msg.getData().getString(Constants.DEVICE_NAME);
-//                    if (null != activity) {
-//                        Toast.makeText(activity, "Connected to "
-//                                + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
-//                    }
-                    break;
-                case Constants.MESSAGE_TOAST:
-//                    if (null != activity) {
-//                        Toast.makeText(activity, msg.getData().getString(Constants.TOAST),
-//                                Toast.LENGTH_SHORT).show();
-//                    }
-                    break;
-            }
-        }
-    };
-    /**
-     * Local Bluetooth adapter
-     */
-    private BluetoothAdapter mBluetoothAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -90,13 +23,6 @@ public class MainActivity extends ActionMenuActivity {
         setContentView(R.layout.activity_main);
 
         user_name = findViewById(R.id.user_name);
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
-        // If the adapter is null, then Bluetooth is not supported
-        if (mBluetoothAdapter == null) {
-            System.out.println("BLUETOOTH IS NOT AVAILABLE");
-            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
-        }
     }
 
     @Override
@@ -116,21 +42,8 @@ public class MainActivity extends ActionMenuActivity {
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos); //bm is the bitmap object
             byte[] b = baos.toByteArray();
-            System.out.println("=========1");
-            BluetoothDevice device = mBluetoothAdapter.getRemoteDevice("98:09:CF:7D:46:CB");
-            BluetoothChatService bluetoothChatService = new BluetoothChatService(this, mHandler);
-            System.out.println("=========2");
-//            bluetoothChatService.start();
-            bluetoothChatService.connect(device, true);
-            System.out.println(device.getAddress());
-            System.out.println("=========3");
-            bluetoothChatService.write("hbi".getBytes());
-            System.out.println("=========4");
-//            bluetoothChatService.stop();
-
-
+            //todo bluetooth here: send b to phone
             user_name.setText("Hi, my name is EraO");
-//            imageView.setImageBitmap(imageBitmap);
         }
     }
 
