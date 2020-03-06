79d3da8542f7076907831786b69fad45349271c4
fredpan <fredpan0821@gmail.com>
2020-03-06 00:16:21 -0500

    temp_bluetooth

---
+++ app/src/main/java/tech/erao/prosopagnosiahelperdisplayer/PicAutoCapture.java
@@ -0,0 +1,90 @@
+package tech.erao.prosopagnosiahelperdisplayer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+@SuppressWarnings("deprecation")
+public class PicAutoCapture extends Activity implements SurfaceHolder.Callback {
+
+    private Camera mCamera;
+    private SurfaceView surfaceView;
+    private SurfaceHolder surfaceHolder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.camera_layout);
+        surfaceView = findViewById(R.id.surfaceview);
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(PicAutoCapture.this);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        try {
+            mCamera = Camera.open();
+            mCamera.setPreviewDisplay(surfaceHolder);
+            mCamera.startPreview();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        capture();
+    }
+
+    private void capture() {
+        mCamera.takePicture(null, null, null, new Camera.PictureCallback() {
+
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+                Toast.makeText(getApplicationContext(), "Picture Taken",
+                        Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent();
+                intent.putExtra("image_arr", data);
+                setResult(RESULT_OK, intent);
+                camera.stopPreview();
+                if (camera != null) {
+                    camera.release();
+                    mCamera = null;
+                }
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                               int height) {
+        Log.e("Surface Changed", "format   ==   " + format + ",   width  ===  "
+                + width + ", height   ===    " + height);
+        try {
+            mCamera.setPreviewDisplay(holder);
+            mCamera.startPreview();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        Log.e("Surface Created", "");
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        Log.e("Surface Destroyed", "");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mCamera != null) {
+            mCamera.stopPreview();
+            mCamera.release();
+        }
+    }
+}
