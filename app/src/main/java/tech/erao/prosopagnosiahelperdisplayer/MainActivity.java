893be45e831c64098e34335baf2de6f3446b3586
yixiaohong <hongyixiao@outlook.com>
2020-04-02 15:44:20 -0400

    glass can upload image to google storage after click

--- app/src/main/java/tech/erao/prosopagnosiahelperdisplayer/MainActivity.java
+++ app/src/main/java/tech/erao/prosopagnosiahelperdisplayer/MainActivity.java
@@ -4,10 +4,23 @@ package tech.erao.prosopagnosiahelperdisplayer;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.vuzix.hud.actionmenu.ActionMenuActivity;
 
 import java.io.ByteArrayOutputStream;
@@ -16,6 +29,7 @@ import java.io.ByteArrayOutputStream;
 public class MainActivity extends ActionMenuActivity {
     private TextView user_name;
     private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,8 +57,53 @@ public class MainActivity extends ActionMenuActivity {
             bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos); //bm is the bitmap object
             byte[] b = baos.toByteArray();
             //todo bluetooth here: send b to phone
-            user_name.setText("Hi, my name is EraO");
+            uploadImage(bmp);
+
+
+            user_name.setText("Hello");
         }
+    }
+
+    private void uploadImage(Bitmap bitmap) {
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        final StorageReference storageRef = mStorageRef.child("photos/test.jpg");
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 20, baos);
+        byte[] data = baos.toByteArray();
+
+        final UploadTask uploadTask = storageRef.putBytes(data);
+        uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(getApplicationContext(), "Photo Uploaded",
+                        Toast.LENGTH_SHORT).show();
+
+                uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                    @Override
+                    public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                        if (!task.isSuccessful()) {
+                            throw task.getException();
+                        }
+                        return storageRef.getDownloadUrl();
+                    }
+                }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Uri> task) {
+                        if (task.isSuccessful()) {
+                            Uri downUri = task.getResult();
+                            Log.d("Final URL", "onComplete: Url: " + downUri.toString());
+                        }
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "Photo Uploading Failed",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void dispatchTakePictureIntent() {
