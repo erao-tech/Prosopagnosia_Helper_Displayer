a11299885c5f4a20cf16cdba7a0a773630ce9a47
yixiaohong <hongyixiao@outlook.com>
2020-04-03 02:07:34 -0400

    http request

--- app/src/main/java/tech/erao/prosopagnosiahelperdisplayer/MainActivity.java
+++ app/src/main/java/tech/erao/prosopagnosiahelperdisplayer/MainActivity.java
@@ -6,30 +6,75 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Debug;
+import android.os.Environment;
+import android.util.Base64;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
+import com.android.internal.http.multipart.FilePart;
+import com.android.internal.http.multipart.MultipartEntity;
+import com.android.internal.http.multipart.Part;
+import com.android.internal.http.multipart.StringPart;
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabel;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabeler;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.vuzix.hud.actionmenu.ActionMenuActivity;
 
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpException;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.CoreProtocolPNames;
+import org.apache.http.util.EntityUtils;
+
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @SuppressWarnings("deprecation")
 public class MainActivity extends ActionMenuActivity {
     private TextView user_name;
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private StorageReference mStorageRef;
+    private FirebaseAuth myFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +82,25 @@ public class MainActivity extends ActionMenuActivity {
         setContentView(R.layout.activity_main);
 
         user_name = findViewById(R.id.user_name);
+//        myFirebaseAuth = FirebaseAuth.getInstance();
+//
+//        String account = "test@test.com";
+//        String pwd = "123123";
+//        if (!account.isEmpty() && !pwd.isEmpty()){
+//            myFirebaseAuth.signInWithEmailAndPassword(account, pwd).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//                @Override
+//                public void onComplete(@NonNull Task<AuthResult> task) {
+//                    if (task.isSuccessful()){
+//                        Toast.makeText(getApplicationContext(), "You are logged in",
+//                                Toast.LENGTH_SHORT).show();
+//                    } else{
+//                        Toast.makeText(getApplicationContext(), task.getResult().toString(),
+//                                Toast.LENGTH_SHORT).show();
+//                    }
+//                }
+//            });
+//        }
+
     }
 
     @Override
@@ -55,55 +119,93 @@ public class MainActivity extends ActionMenuActivity {
             Bitmap bmp = BitmapFactory.decodeByteArray(image, 0, image.length);
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos); //bm is the bitmap object
-            byte[] b = baos.toByteArray();
-            //todo bluetooth here: send b to phone
-            uploadImage(bmp);
+            byte[] imgfile = baos.toByteArray();
+            Toast.makeText(getApplicationContext(), "Finished",
+            Toast.LENGTH_SHORT).show();
+            String url = "http://54.161.20.123:5000/whichface";
+            //todo: Create a POST HTTP REQUEST with url above, convert the bitmap file to .JPG and attach it in request.files with key="img", see ApiTester.py in Flask Server
 
-
-            user_name.setText("Hello");
         }
     }
 
-    private void uploadImage(Bitmap bitmap) {
+
+    private String whichFace(){
+        return "";
+    }
+
+    private void uploadImage(final Bitmap bitmap) {
         mStorageRef = FirebaseStorage.getInstance().getReference();
         final StorageReference storageRef = mStorageRef.child("photos/test.jpg");
 
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.JPEG, 20, baos);
-        byte[] data = baos.toByteArray();
 
-        final UploadTask uploadTask = storageRef.putBytes(data);
-        uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                Toast.makeText(getApplicationContext(), "Photo Uploaded",
-                        Toast.LENGTH_SHORT).show();
+//        final UploadTask uploadTask = storageRef.putBytes(data);
+//        uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//            @Override
+//            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                Toast.makeText(getApplicationContext(), "Photo Uploaded",
+//                        Toast.LENGTH_SHORT).show();
+////                displaytags(bitmap);
+//
+//                uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+//                    @Override
+//                    public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+//                        if (!task.isSuccessful()) {
+//                            throw task.getException();
+//                        }
+//                        return storageRef.getDownloadUrl();
+//                    }
+//                }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Uri> task) {
+//                        if (task.isSuccessful()) {
+//                            Uri downUri = task.getResult();
+//                            Log.d("Final URL", "onComplete: Url: " + downUri.toString());
+//                        }
+//                    }
+//                });
+//            }
+//        }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                Toast.makeText(getApplicationContext(), "Photo Uploading Failed",
+//                        Toast.LENGTH_SHORT).show();
+//            }
+//        });
+    }
 
-                uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+    public void displaytags(Bitmap bitmap){
+        //add tags
+        //imagetag
+        FirebaseVisionImageLabeler labeler = FirebaseVision.getInstance().getCloudImageLabeler();
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+        labeler.processImage(image)
+                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionImageLabel>>() {
                     @Override
-                    public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
-                        if (!task.isSuccessful()) {
-                            throw task.getException();
+                    public void onSuccess(List<FirebaseVisionImageLabel> labels) {
+                        // Task completed successfully
+                        // ...
+                        String output = "";
+                        for (FirebaseVisionImageLabel label: labels) {
+                            String text = label.getText();
+                            String entityId = label.getEntityId();
+                            float confidence = label.getConfidence();
+                            if (confidence>0.7){
+                                output = output + "#" + text;
+                            }
+
                         }
-                        return storageRef.getDownloadUrl();
+                        user_name.setText(output);
                     }
-                }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                })
+                .addOnFailureListener(new OnFailureListener() {
                     @Override
-                    public void onComplete(@NonNull Task<Uri> task) {
-                        if (task.isSuccessful()) {
-                            Uri downUri = task.getResult();
-                            Log.d("Final URL", "onComplete: Url: " + downUri.toString());
-                        }
+                    public void onFailure(@NonNull Exception e) {
+                        // Task failed with an exception
+                        // ...
                     }
                 });
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(getApplicationContext(), "Photo Uploading Failed",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 
     private void dispatchTakePictureIntent() {
